f13866b5676362355a95c124b53e77e04606d0cc
Ana <ana.penarodriguez@deltatre.it>
2020-04-08 09:37:30 +0200

    List filling looks much better now

--- populateTasks.js
+++ populateTasks.js
@@ -34,13 +34,30 @@ function genreateList(list, data) {
     for(let el of data){
         console.log(el.description)
         let li = document.createElement("li")
+        li.className="list-group-item d-flex justify-content-between align-items-center"
         let checkBox = document.createElement("input")
         let label = document.createElement("label")
+        let projectBadge = document.createElement('span')
+        let sharedIcon = document.createElement('span')
+        sharedIcon.className = 'material-icons'
+        sharedIcon.innerText = 'people_outline'
+        projectBadge.className = "badge badge-pill badge-secondary"
+        projectBadge.innerText = el.project
         checkBox.type = "checkbox"
         checkBox.value = el["description"]
         list.appendChild(li)
         li.appendChild(checkBox);
         li.appendChild(label);
         label.appendChild(document.createTextNode(el["description"]))
+        li.appendChild(projectBadge)
+        if (!el.private){
+            li.appendChild(sharedIcon)
+        }
     }
 }
+
+{/* <th scope="row"><input type="checkbox" aria-label="completed"></th>
+<td>Study CSS</td>
+<td><span class="badge badge-pill badge-secondary">Web Applications I</span></td>
+<td><span class="material-icons">people_outline</span></td>
+<td>Today at 14:00</td> */}
